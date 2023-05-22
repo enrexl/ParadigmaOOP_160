05c483e62068fa77de5a2e584cdaffdf001abdbf
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-05-22 15:23:05 +0700

    Menambahkan member public pada class mahasiswa

--- ParadigmaOOP_160/ParadigmaOOP_160.cpp
+++ ParadigmaOOP_160/ParadigmaOOP_160.cpp
@@ -8,8 +8,15 @@ private:
     float nilai;
 
 public:
-    )
-};
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai){
+}
+
+    ~mahasiswa() {
+        cout << "Id      =" << id << endl;
+        cout << "Nama    =" << nama << endl;
+        cout << "Nilai   =" << nilai << endl;
+    }
+}
 int main()
 {
     std::cout << "Hello World!\n";
