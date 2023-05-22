3f663668e3e6d93d546760cbc390cce77c13e27e
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-05-22 16:03:42 +0700

    menghapus variable yang telah dibuat sebelumnya

--- mainA/mainA.cpp
+++ mainA/mainA.cpp
@@ -19,6 +19,15 @@ int main(){
 
     varIbu->cetakAnak();
     varIbu2->cetakAnak();
+
+    delete varIbu;
+    delete varIbu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
+
+    return 0;
 }
+
 
 
