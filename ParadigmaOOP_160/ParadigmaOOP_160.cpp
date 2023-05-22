96e762b2c0c9ed9fbf816f67a5ca7d19f58fd751
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-05-22 15:19:54 +0700

    Membuat class mahasiswa beserta member privatenya

--- ParadigmaOOP_160/ParadigmaOOP_160.cpp
+++ ParadigmaOOP_160/ParadigmaOOP_160.cpp
@@ -1,6 +1,15 @@
-
 #include <iostream>
+using namespace std;
 
+class mahasiswa {
+private:
+    const int id;
+    string nama;
+    float nilai;
+
+public:
+    )
+};
 int main()
 {
     std::cout << "Hello World!\n";
